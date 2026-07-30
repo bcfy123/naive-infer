a7aced0a05b09c102be419ebba81fa2a12fed168
bcfy123 <wjldwdsj@163.com>
2026-07-31 01:51:33 +0800

    add get size test

---
+++ test_size.cpp
@@ -0,0 +1,14 @@
+//
+// Created by wjldw on 2026/7/31.
+//
+#include "tensor.hpp"
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+TEST(test_tensor_size, tensor_size1) {
+    using namespace naive_infer;
+    Tensor<float> f1(2, 3, 4);
+    LOG(INFO) << "-----------------------Tensor Get Size-----------------------";
+    LOG(INFO) << "channels: " << f1.channels();
+    LOG(INFO) << "rows: " << f1.rows();
+    LOG(INFO) << "cols: " << f1.cols();
+}
